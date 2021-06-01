5241007e8e7c5216ea9be71f4827fda979b30e96
md-siam <md.siam03@gmail.com>
2021-06-02 01:40:10 +0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # COVID19_data_scraping
-This will scrap covid19 data from https://www.worldometers.info/coronavirus/
+
+This will scrap covid19 data from http://103.247.238.92/webportal/pages/covid19.php
--- index_v2.js
+++
@@ -1,8 +0,0 @@
-const request = require("request");
-const cheerio = require("cheerio");
-const cron = require("node-cron");
-
-//? Schedule tasks to be run on the server
-// cron.schedule("* * * * *", function () {
-//   console.log("running a task every minute");
-// });
