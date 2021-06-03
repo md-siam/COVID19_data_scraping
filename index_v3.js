cf7a199335cf665ecb1ca5644cb76147e368bef3
md-siam <md.siam03@gmail.com>
2021-06-03 15:47:53 +0600

    Working Email Trigger

--- index_v3.js
+++ index_v3.js
@@ -6,6 +6,7 @@ const cron = require("node-cron");
 //? Schedule tasks to be run on the server
 // cron.schedule("* * * * *", function () {
 //   console.log("running a task every minute");
+//
 // });
 
 async function main() {
